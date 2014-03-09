4949e24512715a2c91ffe566d8710ff858b7c371
duncan <djames3107@googlemail.com>
2014-03-09 11:22:29 +0000

    Updates as of 9/3/14

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.configure(function() {
 
 });
 var watchList = {
+		type: "company",
 	    total: 0,
 	    tweetScore: {},
 	    symbols: {},
@@ -33,73 +34,36 @@ var watchList = {
 	    initialScores: {},
 	    initialPrices: {},
 	    prices: {},
-	    streaming: false
+	    wordcloud: "all",
+	    topWords: {},
+	    wordCloudFrozen: "false"
 };
-var streaming = false;
-request.get('http://localhost:8080/twittertrader/company/getAllCompanies',function(error, response, body){
-	if(error){
-		console.log(error);
-	}else if (!error && response.statusCode != 200) {
-		console.log("There has been a problem"+ response.statusCode);
-    }else {
-    	companies = JSON.parse(body);
-    	_.each(companies, function(v) {
-    		watchList.tweetScore[v.stockSymbol] = 0;
-    		watchList.symbols[v.stockSymbol] = v.companyScore;
-    		watchList.initialScores[v.stockSymbol] = v.companyScore;
-    		watchList.prices[v.stockSymbol]=v.stockPrice;
-    		watchList.initialPrices[v.stockSymbol]=v.stockPrice;
-    		watchList.lastTweetTime[v.stockSymbol]= new Date();
-    	});
-    	console.log("Companies successfully received");
-    }	
-});
-    
+var watchListIndustries = {
+		type: "industry",
+	    total: 0,
+	    tweetScore: {},
+	    symbols: {},
+	    lastTweetTime: {},
+	    initialScores: {},
+	    initialPrices: {},
+	    prices: {},
+	    topWords: {},
+	    wordcloud: "all",
+	    pricesIndividualCompanies: {},
+	    topWordsIndividualCompanies:{},
+	    wordCloudFrozen: "false"
+};
+var currentWatchList = watchList;
+  
 app.get('/', function(req, res) {
-	if(!watchList.streaming==true){
-		request.get('http://localhost:8080/twittertrader/twitter/start/'+3,
-			function(error, response, body){
-				if(error){
-					console.log(error);
-					res.render('500.jade', {
-						title : 'Welcome to TwitterTrader'
-					});
-				}else if (!error && response.statusCode != 200) {
-					
-					res.render('500.jade', {
-						title : 'Welcome to TwitterTrader'
-					});
-		        }else {
-		        	watchList.streaming=true;
-		        	res.render('index', { data: watchList });
-		        }
-		});
-	} else {
-		res.render('index', { data: watchList });
-	}
+	currentWatchList = watchList;
+	res.render('index', { data: currentWatchList  });
+	sockets.sockets.emit('wordcloud', currentWatchList);
 });
-app.get('/stopStreaming', function(req, res){
-	if(watchList.streaming){
-		request.get('http://localhost:8080/twittertrader/twitter/stop',
-				function(error, response, body){
-					if(error){
-						console.log(error);
-						res.render('500.jade', {
-							title : 'Welcome to TwitterTrader'
-						});
-					}else if (!error && response.statusCode != 200) {
-						
-						res.render('500.jade', {
-							title : 'Welcome to TwitterTrader'
-						});
-			        }else {
-			        	watchList.streaming=false;
-			        	res.render('index', { data: watchList });
-			        }
-			});
-	}else {
-		res.render('index', { data: watchList });
-	}
+app.get('/industries', function(req, res) {
+	currentWatchList = watchListIndustries;
+	res.render('index', { data: currentWatchList });
+	sockets.sockets.emit('wordcloud', currentWatchList);
 });
 app.rabbitMqConnection = amqp.createConnection({
 	host : 'localhost'
@@ -115,21 +79,127 @@ app.rabbitMqConnection.on('ready', function() {
 	  	  watchList.symbols[update.companySymbol] += update.tweetScore;
 	  	  watchList.lastTweetTime[update.companySymbol] = new Date().getTime();
 		  watchList.total++;
-		  sockets.sockets.emit('data', watchList);
+		  watchListIndustries.tweetScore[update.industry] = update.tweetScore;	
+		  watchListIndustries.symbols[update.industry] += update.tweetScore;
+		  watchListIndustries.lastTweetTime[update.industry] = new Date().getTime();
+		  watchListIndustries.total++;
+		  sockets.sockets.emit('data', currentWatchList);
 		}
 		if(update.type == "stockPrice"){
 			watchList.prices[update.companySymbol] = update.stockPrice;
-			sockets.sockets.emit('data', watchList);
+			watchListIndustries.pricesIndividualCompanies[update.industry].prices[update.companySymbol] = update.stockPrice;
+			watchListIndustries.prices[update.industry]=0;
+			_.each(watchListIndustries.pricesIndividualCompanies, function(element,index,industries) {
+				if(index == update.industry){
+	    			_.each(element.prices, function(element2,index2, industries2){
+		    			watchListIndustries.prices[index]+=element2;
+		    		});
+	    		}
+			});
+			sockets.sockets.emit('data', currentWatchList);
+		}
+		if(update.type == "topWords"){
+			watchList.topWords[update.companySymbol] = update.topWords;
+			watchListIndustries.topWordsIndividualCompanies[update.industry].topWords[update.companySymbol]=update.topWords;
+			watchListIndustries.topWords[update.industry]=[];
+			_.each(watchListIndustries.topWordsIndividualCompanies, function(element,index,industries) {
+				if(index == update.industry){
+	    			_.each(element.topWords, function(element2,index2, industries2){
+		    			_.each(element2, function(element3,index3,industries3){
+		    				
+		    				watchListIndustries.topWords[index].push(element3)
+		    			})
+		    		});
+	    		}
+			});
+			if(currentWatchList.wordCloudFrozen=="false"){
+				sockets.sockets.emit('wordcloud', currentWatchList)
+			}
+			
 		}
 	});
 });
 var server = http.createServer(app);
 var sockets = io.listen(server);
 sockets.set('log level',1);
-//If the client just connected, give them fresh data!
+// If the client just connected, give them fresh data!
 sockets.sockets.on('connection', function(socket) { 
-  socket.emit('data', watchList);
+	socket.on('changeWordCloud',function(data){
+		currentWatchList.wordcloud = data.id;
+		socket.emit('wordcloud', currentWatchList);
+	});
+	socket.on('freezeWordCloud',function(){
+		if(watchList.wordCloudFrozen == "false"){
+			watchList.wordCloudFrozen = "true"
+			watchListIndustries.wordCloudFrozen = "true";
+		}else {
+			watchList.wordCloudFrozen = "false"
+			watchListIndustries.wordCloudFrozen = "false";
+		}
+	});
+  socket.emit('data', currentWatchList);
 });
+request.get('http://localhost:8080/twittertrader/company/getAllCompanies',function(error, response, body){
+	if(error){
+		console.log(error);
+	}else if (!error && response.statusCode != 200) {
+		console.log("There has been a problem"+ response.statusCode);
+    }else {
+    	var industryList = [];
+    	companies = JSON.parse(body);
+    	_.each(companies, function(v) {
+    		watchList.tweetScore[v.stockSymbol] = 0;
+    		watchList.symbols[v.stockSymbol] = v.companyScore;
+    		watchList.initialScores[v.stockSymbol] = v.companyScore;
+    		watchList.prices[v.stockSymbol]=v.stockPrice;
+    		watchList.initialPrices[v.stockSymbol]=v.stockPrice;
+    		watchList.lastTweetTime[v.stockSymbol]= new Date();
+    		watchList.topWords[v.stockSymbol]= {};
+    		
+    		// maybe an ifstatement to initialise if previously not set
+    		if(_.contains(industryList, v.industry)){
+    			watchListIndustries.symbols[v.industry] +=v.companyScore;
+        		watchListIndustries.initialScores[v.industry] +=v.companyScore;
+        		watchListIndustries.initialPrices[v.industry]+=v.stockPrice;
+        		watchListIndustries.pricesIndividualCompanies[v.industry].prices[v.stockSymbol]=v.stockPrice;
+        		watchListIndustries.topWordsIndividualCompanies[v.industry].topWords[v.stockSymbol]={};
+    		} else{
+    			watchListIndustries.tweetScore[v.industry]=0;
+    			watchListIndustries.symbols[v.industry] =v.companyScore;
+        		watchListIndustries.initialScores[v.industry] =v.companyScore;
+        		if(! watchListIndustries.pricesIndividualCompanies[v.industry] )
+        		  watchListIndustries.pricesIndividualCompanies[v.industry] ={prices:{}};
+        		watchListIndustries.pricesIndividualCompanies[v.industry].prices[v.stockSymbol]=v.stockPrice;
+        		if(! watchListIndustries.topWordsIndividualCompanies[v.industry] )
+          		  watchListIndustries.topWordsIndividualCompanies[v.industry] ={topWords:{}};
+          		watchListIndustries.topWordsIndividualCompanies[v.industry].topWords[v.stockSymbol]={};
+        		watchListIndustries.initialPrices[v.industry]=v.stockPrice;
+        		watchListIndustries.lastTweetTime[v.industry]= new Date();
+        		watchListIndustries.topWords[v.industry]= [];
+        		watchListIndustries.prices[v.industry]= 0;
+        		industryList.push(v.industry);
+    		}
+    		
+    	});
+    	_.each(watchListIndustries.pricesIndividualCompanies, function(element,index,industries) {
+    		_.each(element.prices, function(element2,index2, industries2){
+    			watchListIndustries.prices[index]+=element2;
+    		});
+		});
+    	console.log("Companies successfully received");
+    	request.get('http://localhost:8080/twittertrader/twitter/start/'+3,
+    			  function(error, response, body){
+    				if(error){
+    					console.log(error);
+    				}else if (!error && response.statusCode != 200) {
+    					console.log(response);
+    			    }else{
+    			    	console.log("Successfully started streaming");
+    			    }
+    	});
+    }	
+});
+  
 server.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,32 +1,113 @@
 $(function() {
-    var socket = io.connect(window.location.hostname);
-    socket.on('data', function(data) {
-        var total = data.total;
-        for (var key in data.symbols) {
-            var val = data.tweetScore[key];
-            if (isNaN(val)) {
-                val = 0;
-            }
-            var previousTweetTime = data.lastTweetTime[key];
-            var currentTime = new Date().getTime();
-            var differenceInSeconds = (currentTime - previousTweetTime)/1000;
-            if( val < 0) {
-              $('li[data-symbol="' + key + '"]').each(function() {
-                $(this).css('background-color', 'rgb('+255/(differenceInSeconds*100)+',0,0)');
-              });
-            } else if(val > 0){
-            	$('li[data-symbol="' + key + '"]').each(function() {
-                    $(this).css('background-color', 'rgb(0,0,'+255/(differenceInSeconds*100)+')');
-                  });
-            } else{
-            	$('li[data-symbol="' + key + '"]').each(function() {
-                    $(this).css('background-color', 'rgb(0,0,0)');
-                  });
-            }
-            $('#stockPrice'+key).text(data.prices[key].toFixed(2));
-            $('#stockPriceChange'+key).text("("+(((data.prices[key]-data.initialPrices[key])/data.initialPrices[key])*100).toFixed(2)+"%)");
-            $('#scoreChange'+key).text("("+(((data.symbols[key]-data.initialScores[key])/data.initialScores[key])*100).toFixed(2)+"%)");
-        }
-        $('#last-update').text(new Date().toTimeString());
-    });
+	var socket = io.connect(window.location.hostname);
+	socket
+			.on(
+					'data',
+					function(data) {
+						$('li').removeClass('active');
+						if(data.type=='company'){
+							$('li[id="company"]').addClass('active');
+						}else{
+							$('li[id="industry"]').addClass('active');
+						}
+						if(data.wordCloudFrozen=="true"){
+							$('li[id="freeze"]').addClass('active');
+						}
+						$('li[id="'+data.wordcloud+'"]').addClass('active');
+						var total = data.total;
+						var wordcloudList = [];
+						for ( var key in data.symbols) {
+							var val = data.tweetScore[key];
+							if (isNaN(val)) {
+								val = 0;
+							}
+							var previousTweetTime = data.lastTweetTime[key];
+							var currentTime = new Date().getTime();
+							var differenceInMilliSeconds = (currentTime - previousTweetTime) / 10;
+							if (val < 0) {
+								$('liA[data-symbol="' + key + '"]')
+										.each(
+												function() {
+													$(this)
+															.css(
+																	'background-color',
+																	'rgb('
+																			+ 255
+																			/ differenceInMilliSeconds
+																			+ ',0,0)');
+												});
+							} else if (val > 0) {
+								$('liA[data-symbol="' + key + '"]')
+										.each(
+												function() {
+													$(this)
+															.css(
+																	'background-color',
+																	'rgb(0,0,'
+																			+ 255
+																			/ differenceInMilliSeconds
+																			+ ')');
+												});
+							} else {
+								$('liA[data-symbol="' + key + '"]').each(
+										function() {
+											$(this).css('background-color',
+													'rgb(0,0,0)');
+										});
+							}
+							$('#stockPrice' + key).text(
+									data.prices[key].toFixed(2));
+							$('#stockPriceChange' + key)
+									.text(
+											"("
+													+ (((data.prices[key] - data.initialPrices[key]) / data.initialPrices[key]) * 100)
+															.toFixed(2) + "%)");
+							$('#scoreChange' + key)
+									.text(
+											"("
+													+ (((data.symbols[key] - data.initialScores[key]) / data.initialScores[key]) * 100)
+															.toFixed(2) + "%)");
+
+						}
+						$('#last-update').text(new Date().toTimeString());
+
+					});
+	socket.on('wordcloud', function(data) {
+		$('li').removeClass('active');
+		if(data.type=='company'){
+			$('li[id="company"]').addClass('active');
+		}else{
+			$('li[id="industry"]').addClass('active');
+		}
+		if(data.wordCloudFrozen=="true"){
+			$('li[id="freeze"]').addClass('active');
+		}
+		$('li[id="'+data.wordcloud+'"]').addClass('active');
+		var wordcloudList = [];
+		for ( var key in data.symbols) {
+			// if key is equal to current tab name or tab name is all
+			// then push onto list
+			if (data.wordcloud == "all" || key == data.wordcloud) {
+				for ( var item in data.topWords[key]) {
+					
+					for ( var temp in data.topWords[key][item]) {
+						wordcloudList.push([ temp,
+								data.topWords[key][item][temp] ]);
+						console.log(temp);
+					}
+				}
+			}
+		}
+		WordCloud(document.getElementById('canvas'), {
+			list : wordcloudList
+		});
+	})
 })
+function MyFunction(value){
+	var socket = io.connect(window.location.hostname);
+	socket.emit('changeWordCloud',{id: value});
+}
+function Freeze(){
+	var socket = io.connect(window.location.hostname);
+	socket.emit('freezeWordCloud');
+}
